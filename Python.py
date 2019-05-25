7b2e781cd890a14c4b41b81696b2673b216cb9e0
Rommie1983 <rommie1983@gmail.com>
2019-05-25 16:07:14 +0200

    My Django Girls app, first commit

---
+++ Python.py
@@ -0,0 +1,18 @@
+def hi():
+    print('Hi there!')
+    print('How are you?')
+
+def hallo(name):
+	if name == 'Ola':
+		print('Hey Ola!')
+	elif name == 'Sonja':
+		print('Hey Sonja!')
+	else:
+		print('Hey anonymous!')
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+squares = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for number in range(1, 10):
+    print(number)
